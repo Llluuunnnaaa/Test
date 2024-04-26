cbe58ffda3c70e6504da1ebee411fe89cb5dbe75
Llluuunnnaaa <angelkova@inbox.ru>
2024-04-26 19:14:27 +0300

    Release Test1

---
+++ practice/script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('partnerModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('partnerModal').style.display = 'none';
+}
