b69afcfde0eb5f692fc05ea36213ae3454c165e1
Llluuunnnaaa <167817770+Llluuunnnaaa@users.noreply.github.com>
2024-04-28 16:43:45 +0300

    Delete practice/script.js

--- practice/script.js
+++
@@ -1,7 +0,0 @@
-function openModal() {
-    document.getElementById('partnerModal').style.display = 'block';
-}
-
-function closeModal() {
-    document.getElementById('partnerModal').style.display = 'none';
-}
